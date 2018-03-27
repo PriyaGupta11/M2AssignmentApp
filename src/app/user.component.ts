4ff916f0d2394e942a8ad2b0c8347e8253558479
PriyaGupta11 <37463427+PriyaGupta11@users.noreply.github.com>
2018-03-27 01:01:47 -0700

    Add files via upload

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,32 +1,5 @@
-// import {Component,EventEmitter,Input,Output,NgModule} from '@angular/core';
 
-// import {BrowserModule} from '@angular/platform-browser';
-
-// import { FormsModule } from '@angular/forms';
-
-// @Component({
-//     selector:"user-comp",
-
-//     templateUrl:"./user.component.html",
-//     styleUrls:["./user.component.css"]
-
-// })
-
-
-// export class userComponent{
-//     @Input('parentData') incomingData: string;
-//     @Output('childData') outgoingData = new EventEmitter<string>();
-//     childSampleData :string = "Some child Data";
-//     constructor() { }
-
-// 	public sendData(data:any){
-// 		this.outgoingData.emit(data);
-//     } 
-// }
-
-
-
-import { Component, OnInit,Output,EventEmitter,Input, OnChanges } from '@angular/core';  
+import { Component, OnInit,Output,EventEmitter,Input } from '@angular/core';  
 @Component({  
   selector: 'user-comp',  
   templateUrl:"./user.component.html",
@@ -34,19 +7,20 @@ import { Component, OnInit,Output,EventEmitter,Input, OnChanges } from '@angular
 })  
 export class userComponent implements OnInit {  
 
-@Input() name: string;
-
-  @Input() result:string;  // it will receive data
+  @Input() resultAr:any[]; 
   @Output() clicked=new EventEmitter<string>();  
   constructor() { }  
   ngOnInit() {  
   }  
 onClick(searchTerm:string){  
-this.clicked.emit(searchTerm);  
+
+ //this.clicked.emit(searchTerm);  
+if(searchTerm!=''){  
+  this.resultAr=this.resultAr.filter(res=>res.userName.startsWith(searchTerm));  
+  }  
+  else{  
+    this.resultAr=this.resultAr;
+     } 
 }  
 }
 
-// export class userComponent implements OnChanges{
-
-
-// }
